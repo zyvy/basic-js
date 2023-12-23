91f7bcf34b225a2fa939901608b6ec891469610d
zyvy <gm.zyvy@gmail.com>
2023-12-24 00:15:42 +0300

    feat: fix recursive depth

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -18,44 +18,27 @@ class DepthCalculator {
     this.currentFloor = 1;
   }
   calculateDepth(arr) {
+    let myMax = 1;
     //console.log('started', arr[0], arr.length);
     for (let i = 0; i < arr.length; i++) {
-    //  console.log("enter with", arr[i]);
-   //   console.log("max is", this.maxDepth);
+ //     console.log("enter with", arr[i]);
+  //    console.log("max is", myMax);
       if (Array.isArray(arr[i])) {
-     //   console.log("found new", arr[i]);
-        this.currentFloor += 1;
-     //   console.log("go to fllor", this.currentFloor);
-        this.calculateDepth(arr[i]);
-      } else if (this.currentFloor > 1 && i === arr.length - 1) {
-        // добежали до конца текущего и уходим на уровень назад
-        if (this.currentFloor > this.maxDepth) {
-          this.maxDepth = this.currentFloor;
-      //    console.log("max is", this.maxDepth);
-        }
-        this.currentFloor -= 1;
-      //  console.log('go bottom 1', this.currentFloor);
+  //      console.log("found new", arr[i]);
+        myMax = Math.max(myMax, this.calculateDepth(arr[i]) + 1)
+  //      console.log("go to fllor", this.currentFloor);
       }
-      // если пустой
-      else if (this.currentFloor > 1 && arr.length === 0) {
-        if (this.currentFloor > this.maxDepth) {
-          this.maxDepth = this.currentFloor;
-     //     console.log("max is", this.maxDepth);
-        }
-        this.currentFloor -= 1;
-     //   console.log('go bottom 2', this.currentFloor);
-      }
-    }
-    // remove line with error and write your code here
-    this.currentFloor = 1;
-    return this.maxDepth;
+     
+    
   }
+  return myMax;
+}
 }
 const depthCalc = new DepthCalculator();
-//console.log(depthCalc.calculateDepth([1, 2, 3,4]))
+console.log(depthCalc.calculateDepth([1, 2, 3,4]))
 //console.log(depthCalc.calculateDepth([1, [[[[[]]]]], 9, [0, [[]]]]));
-//console.log(depthCalc.calculateDepth([1, [8, [[]]], [[[[[[[[[[[[[[[[[[[[[[[[[[[[[[]]]]]]], []]]], []]]]]]]]], []]]], []]]]]]]]]], 2, 3, [8, [[[[[[[[[[[[[[]]]]]]]]]]]]]]], [8, [[[[[[[[[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]]]]]]]]], 4, 5, ['6575', ['adas', ['dfg', [0]]]]]))
-//console.log(depthCalc.calculateDepth([1, 2, 3, [4, 5, [9]], 6, [7,8]]))
+console.log(depthCalc.calculateDepth([1, [8, [[]]], [[[[[[[[[[[[[[[[[[[[[[[[[[[[[[]]]]]]], []]]], []]]]]]]]], []]]], []]]]]]]]]], 2, 3, [8, [[[[[[[[[[[[[[]]]]]]]]]]]]]]], [8, [[[[[[[[[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]]]]]]]]], 4, 5, ['6575', ['adas', ['dfg', [0]]]]]))
+console.log(depthCalc.calculateDepth([1, 2, 3, [4, 5, [9]], 6, [7,8]]))
 module.exports = {
   DepthCalculator,
 };
