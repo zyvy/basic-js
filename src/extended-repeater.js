2567cb56bc37e38fe9e5052cbfb1784bb1b2d3cb
zyvy <gm.zyvy@gmail.com>
2023-12-23 23:36:49 +0300

    feat: fix repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,58 +1,66 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Create a repeating string based on the given parameters
- *  
+ *
  * @param {String} str string to repeat
- * @param {Object} options options object 
+ * @param {Object} options options object
  * @return {String} repeating string
- * 
+ *
  *
  * @example
- * 
- * repeater('STRING', { repeatTimes: 3, separator: '**', 
+ *
+ * repeater('STRING', { repeatTimes: 3, separator: '**',
  * addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
 function repeater(str, options) {
-  params = JSON.parse(JSON.stringify(options));
-  // все в строки
   let newstr = String.toString(str);
-  if (!params.addition){
-    params.addition = '';
+  let repeatTimes = String(options.repeatTimes);
+  let separator = "+";
+  if (String(options.separator) != "undefined") {
+    separator = String(options.separator);
   }
-  params.addition = String(params.addition);
-  if (!params.separator) {
-    params.separator = '+'
+  let addition = "";
+  if (String(options.addition) != "undefined") {
+    addition = String(options.addition);
   }
-  params.separator = String(params.separator);
-  if (!params.additionSeparator) {
-    params.additionSeparator = '|'
+  let additionRepeatTimes = 1;
+  if (options.additionRepeatTimes) {
+    additionRepeatTimes = options.additionRepeatTimes;
   }
-  params.additionSeparator = String(params.additionSeparator);
-  params.repeatTimes = Number.parseInt(params.repeatTimes);
-  params.additionRepeatTimes = Number.parseInt(params.additionRepeatTimes);
+  let additionSeparator = "|";
+  if (String(options.additionSeparator) != "undefined") {
+    additionSeparator = String(options.additionSeparator);
+  }
 
-
-
- // console.log(params);
- // console.log(params.addition);
+  // console.log(params);
+  // console.log(params.addition);
 
   // функция построения повторений
-  function letsBuid(myString, count, delimeter){
-    return myString.concat(delimeter).repeat(count-1).concat(myString)
+  function letsBuid(myString, count, delimeter) {
+    return myString
+      .concat(delimeter)
+      .repeat(count - 1)
+      .concat(myString);
   }
 
   // сооружаем addition
-  let baseAddition = letsBuid(params.addition, params.additionRepeatTimes, params.additionSeparator);
+  let baseAddition = letsBuid(addition, additionRepeatTimes, additionSeparator);
   //сооружаем базовую строку
-  let baseStr = letsBuid(String(str).concat(baseAddition), params.repeatTimes, params.separator);
+  let baseStr = letsBuid(
+    String(str).concat(baseAddition),
+    repeatTimes,
+    separator
+  );
   return baseStr;
   // remove line with error and write your code here
 }
-//console.log(repeater('la', { repeatTimes: 3 }));
-//console.log(repeater('STRING', { repeatTimes: 3, separator: '**', addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' }));
+// console.log(repeater('la', { repeatTimes: 3 }));
+// console.log(repeater('STRING', { repeatTimes: 3, separator: '**', addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' }));
+// console.log(repeater(-222, { repeatTimes: 4, separator: '||', addition: new Map(), additionRepeatTimes: 3, additionSeparator: '&&' }));
+// console.log('-222[object Map]&&[object Map]&&[object Map]||-222[object Map]&&[object Map]&&[object Map]||-222[object Map]&&[object Map]&&[object Map]||-222[object Map]&&[object Map]&&[object Map]')
 module.exports = {
-  repeater
+  repeater,
 };
